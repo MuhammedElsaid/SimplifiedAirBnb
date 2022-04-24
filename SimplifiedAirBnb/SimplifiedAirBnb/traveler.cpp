34fe096db24af854f783817d87ed78d445044383
yara1611 <76455290+yara1611@users.noreply.github.com>
2022-04-24 15:52:51 +0200

    Search Function
    Co-Authored-By: N16-E <103610319+N16-E@users.noreply.github.com>

--- SimplifiedAirBnb/SimplifiedAirBnb/traveler.cpp
+++ SimplifiedAirBnb/SimplifiedAirBnb/traveler.cpp
@@ -5,7 +5,71 @@
 
 using namespace std;
 
-travelers::travelers() {
+Traveler::Traveler() {
 	Email = "";
 	password = "";
+	city = " ";
+	Sday = 0; Eday = 0; Smon = 0; Emon = 0; Syear = 0; Eyear = 0;
+	duration = 0;
+	Hprice = 0.0; Lprice = 0.0; price = 0.0; priceperday = 0.0;
 };
+void Traveler::Search() {
+	cout << "Enter Highest Price: "; cin >> Hprice;
+	cout << "Enter Lowest Price: "; cin >> Lprice;
+	cout << "Results:\n";
+	PriceSearch(Hprice,Lprice);
+	cout << "Enter City: "; cin >> city;
+	cout << "Results:\n";
+	CitySearch(city);
+	cout << "Stay Details:\n";
+	cout<< "Start Day and End Day: "; cin >> Sday>>Eday;
+	cout << "Enter Start Month and End Month: "; cin >> Smon>>Emon;
+	cout << "Enter Start Year and End Year: "; cin >> Syear>>Eyear;
+	cout << "Results:\n";
+	DateSearch();
+};
+
+
+void Traveler::Show() {};
+
+//Should remove duplicates in results
+void Traveler::RemoveDup() {};
+
+
+double Traveler::CalcTotalPrice(double priceperday) {
+	//priceperday is the apartment price per day
+	price = priceperday * duration;
+	return price;
+};
+
+
+void Traveler::DateSearch() {
+	int Dm = (Emon - Smon);
+	int Dy = (Eyear - Syear);
+	duration = (Eday - Sday)+30*Dm+365*Dy;
+};
+
+void Traveler::CitySearch(string cityy) {
+	//City attribute of Apartments should replace Cities
+	for (int i = 0; i < cities.size(); i++) {
+		if (city._Equal(cities[i]))
+			cities[i].pop_back();
+	}
+
+
+};
+
+
+void Traveler::PriceSearch(double hp,double lp) {
+	hp= Hprice; lp = Lprice;
+	while (price <= hp && price >= lp) {
+		//prices of Apartment should replace Prices[i]
+		for(int i=0; i<prices.size(); i++){
+			if (price== prices[i]) {
+                prices.pop_back();
+		      }
+		}
+	}
+};
+
+Traveler::~Traveler(void) {};
--- SimplifiedAirBnb/SimplifiedAirBnb/traveler.h
+++ SimplifiedAirBnb/SimplifiedAirBnb/traveler.h
@@ -1,11 +1,29 @@
 #pragma once
 #include<iostream>
+#include <vector>
 using namespace std;
 
-class travelers {
+class Traveler {
+
+	int Sday, Eday,Smon,Emon,Syear,Eyear, duration;
+	double Hprice, Lprice, price, priceperday;
+	string city;
+	//The Vectors should be replaced with Apartment Info
+	vector <string> cities;
+	vector <double> prices;
+	vector<int> dates;
 public:
 	string Email;
 	string password;
-	travelers();
+	Traveler(void);
+	//Search Functions
+	void Search();
+	void Show();
+	void RemoveDup();
+	double CalcTotalPrice(double);
+	void DateSearch();
+	void CitySearch(string city);
+	void PriceSearch(double,double);
+	~Traveler(void);
 
 };
